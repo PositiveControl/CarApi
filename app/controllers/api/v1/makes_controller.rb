8199302b0bea3916a2a4bd0d1ff25f14fd6473d6
Mark Evans <mark.evans3@redcross.org>
2018-05-03 12:02:15 -0600

    Refactor controllers

--- app/controllers/api/v1/makes_controller.rb
+++ app/controllers/api/v1/makes_controller.rb
@@ -1,49 +1,36 @@
 module Api::V1
   class MakesController < ApplicationController
-    include Pipe
     include CarApiCommon::Controllers
+    include Pipe
 
     def index
-      response = pipe({}, :through => [
+      render pipe({}, :through => [
         :initialize_response, :retrieve_makes, :serialize_makes
       ])
-
-      render response
     end
 
     def create
-      response = pipe(make_params.to_h, :through => [
-        :initialize_response, :build_make, :save_make,
-        :serialize_makes
+      render pipe(make_params.to_h, :through => [
+        :initialize_response, :build_make, :save_make, :serialize_makes
       ])
-
-      render response
     end
 
     def show
-      response = pipe(params[:id], :through => [
+      render pipe(params[:id], :through => [
         :initialize_response, :get_make, :serialize_makes
       ])
-
-      render response
     end
 
     def update
-      response  = pipe(make_params, :through => [
-        :initialize_response, :get_make, :update_make,
-        :serialize_makes
+      render pipe(make_params, :through => [
+        :initialize_response, :get_make, :update_make, :serialize_makes
       ])
-
-      render response
     end
 
     def destroy
-      response = pipe({}, :through => [
-        :initialize_response, :get_make,
-        :destroy_make, :serialize_makes
+      render pipe({}, :through => [
+        :initialize_response, :get_make, :destroy_make, :serialize_makes
       ])
-
-      render response
     end
 
     private
