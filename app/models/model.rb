566f20d294d32115cd813e232ccc73a925e4254e
Mark Evans <mark.evans3@redcross.org>
2018-05-03 12:20:22 -0600

    #7 Creates models table and class

---
+++ app/models/model.rb
@@ -0,0 +1,3 @@
+class Model < ApplicationRecord
+  validates_presence_of :model_title
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180502232955) do
+ActiveRecord::Schema.define(version: 20180503181305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "makes", force: :cascade do |t|
     t.string "brand"
+  end
+
+  create_table "models", force: :cascade do |t|
+    t.string "model_title"
   end
 
   create_table "vehicles", force: :cascade do |t|
